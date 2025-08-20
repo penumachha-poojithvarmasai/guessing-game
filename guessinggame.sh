a9194fb30f18506aeb7c6b26e76b13e4a192798f
Poojith varma sai Penumachha <poojithpenumachha14@gmail.com>
2025-08-20 14:16:59 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,27 @@
+#!/usr/bin/env bash
+
+# Function to count files in current directory
+function file_count {
+    echo $(ls -1 | wc -l)
+}
+
+correct=$(file_count)
+guess=-1
+
+echo "Welcome to the Guessing Game!"
+echo "How many files are in the current directory?"
+
+while [[ $guess -ne $correct ]]
+do
+    read guess
+    if [[ $guess -lt $correct ]]
+    then
+        echo "Too low. Try again:"
+    elif [[ $guess -gt $correct ]]
+    then
+        echo "Too high. Try again:"
+    else
+        echo "🎉 Congratulations! You guessed it right. 🎉"
+    fi
+done
 
